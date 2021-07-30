c8848c31bc6bad9804393ee45303b4555ed75476
leo <leo@lajungle.fr>
2021-07-30 14:47:28 +0200

    password

--- src/router/index.js
+++ src/router/index.js
@@ -7,37 +7,63 @@ const routes = [
   {
     path: '/',
     name: '/',
-    component: () => import(/* webpackChunkName: "gestion" */ '../views/Gestion.vue')
+    component: () => import(/* webpackChunkName: "gestion" */ '../views/Gestion.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/projet',
     name: 'Projet',
-    component: () => import(/* webpackChunkName: "projet" */ '../views/Projet.vue')
+    component: () => import(/* webpackChunkName: "projet" */ '../views/Projet.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/crea',
     name: 'Crea',
-    component: () => import(/* webpackChunkName: "crea" */ '../views/Crea.vue')
+    component: () => import(/* webpackChunkName: "crea" */ '../views/Crea.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/dev',
     name: 'Dev',
-    component: () => import(/* webpackChunkName: "dev" */ '../views/Dev.vue')
+    component: () => import(/* webpackChunkName: "dev" */ '../views/Dev.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/gestion',
     name: 'Gestion',
-    component: () => import(/* webpackChunkName: "gestion" */ '../views/Gestion.vue')
+    component: () => import(/* webpackChunkName: "gestion" */ '../views/Gestion.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/seo',
     name: 'Seo',
-    component: () => import(/* webpackChunkName: "seo" */ '../views/Seo.vue')
+    component: () => import(/* webpackChunkName: "seo" */ '../views/Seo.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/inte',
     name: 'Inte',
-    component: () => import(/* webpackChunkName: "inte" */ '../views/Inte.vue')
+    component: () => import(/* webpackChunkName: "inte" */ '../views/Inte.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/password',
+    name: 'Security',
+    component: () => import(/* webpackChunkName: "inte" */ '../views/Password.vue')
   },
 ]
 
@@ -45,6 +71,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (!storageHelper.getItem('user-password')) next('/password')
+    else next()
+  } else next()
 })
 
 export default router
---
+++ src/views/Password.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+
+        <form v-on:submit.prevent="validateBeforeSubmit">
+            <label class="custom-label control-label">Password</label>
+            <input class="form-control password-field" type="password" name="password" v-model.trim="password">
+            <span class="error help-block" ></span>
+            <div class="text-danger" v-if="error"><p>Incorrect password.</p></div>
+            <button class="btn btn-primary" type="submit">Log in</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+    import storageHelper from 'storage-helper'
+        export default {
+        data () {
+            return {
+                error: null,
+                password: null
+            }
+        },
+        methods: {
+            validateBeforeSubmit () {
+                if (this.password === 'replace-this-with-your-password') {
+                    this.error = false
+                    storageHelper.setItem('user-password', this.password)
+                    router.push('Security')
+                } else {
+                    this.error = true
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+.consigne {
+    font-size: 12px;
+}
+
+</style>
